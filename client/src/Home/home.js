64f34030bc153a045b212678832b82779407b953
BigRicsoo <big.ricsoo@gmail.com>
2021-12-02 18:03:57 +0100

    Add content to the home page

--- client/src/Home/home.js
+++ client/src/Home/home.js
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function Books() {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <h1>Home</h1>
+            Hello There!
+            <br />
+            <br />
+            My goal with this website was to practice, and <i>show off</i> my
+            skills React, MYSQL, Nodejs, Axios etc.
+            <br />
+            <br />
+            The website is a book manager, which allows us to view books data in
+            a table. We cane add new books, update or delete existing ones. Also
+            we can do the same things with book genres, and publishers.
+            <br />
+            <br />
+            The websites style is pretty ugly. That's because my main goal
+            wasn't to make it look nice, but to make it functional.
+            <br />
+            <br />
+            For other proejects i recommend checking out my <a href="https://github.com/BigRicsoo">Github</a>.
+        </>
+    );
 }
